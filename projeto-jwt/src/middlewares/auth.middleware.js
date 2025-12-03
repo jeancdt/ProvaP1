c2a62819178070f4f93f1c5d69f36ab85c94bd7b
jeancdt <flwss157@gmail.com>
2025-12-03 15:02:23 -0300

    update? auth middleware token

--- projeto-jwt/src/middlewares/auth.middleware.js
+++ projeto-jwt/src/middlewares/auth.middleware.js
@@ -3,7 +3,7 @@ const logger = require("../config/logger.config");
 
 function authenticateToken(req, res, next) {
   const authHeader = req.headers["authorization"];
-  const token = authHeader && authHeader.split(" ")[1];
+  const token = authHeader?.split(" ")[1];
 
   if (!token) {
     logger.warn("Tentativa de acesso sem token");
